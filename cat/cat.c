0cd45f332307b2ef2610b7466e598172ea02d8d3
yutarofire <yutaro.ono.418@gmail.com>
2020-09-15 03:16:38 +0900

    Improve cat command implementation, and add tests

--- cat/cat.c
+++ cat/cat.c
@@ -6,10 +6,8 @@
 #include <fcntl.h>
 
 int main(int argc, char **argv) {
-    int fd;
-    if (argc == 1) {
-        fd = 0;
-    } else {
+    int fd = STDIN_FILENO;
+    if (argc > 1) {
         fd = open(argv[1], O_RDONLY);
         if (fd == -1) {
             perror("open");
