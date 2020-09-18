3544de088dfc1596cfde93920f9f3a72bd5441fb
yutarofire <yutaro.ono.418@gmail.com>
2020-09-19 03:43:40 +0900

    Fix indents

--- cat2/cat2.c
+++ cat2/cat2.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  FILE *fd;
-  if (argc == 1) {
-    fd = stdin;
-  } else {
-    fd = fopen(argv[1], "r");
-  }
+    FILE *fd;
+    if (argc == 1) {
+        fd = stdin;
+    } else {
+        fd = fopen(argv[1], "r");
+    }
 
-  int c;
-  while((c = fgetc(fd)) != EOF) {
-    fputc(c, stdout);
-  }
+    int c;
+    while((c = fgetc(fd)) != EOF)
+        fputc(c, stdout);
 
-  exit(0);
+    exit(0);
 }
