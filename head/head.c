2f07a8be7cd68739a6b4415c66789deb066f22ad
yutarofire <yutaro.ono.418@gmail.com>
2020-09-25 02:24:45 +0900

    "head" supports "-n" options

--- head/head.c
+++ head/head.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "at least one argument expected\n");
-        exit(1);
+    int nlines = 10;
+    int opt;
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        switch(opt) {
+        case 'n':
+            nlines = atoi(optarg);
+            break;
+        case '?':
+        default:
+            exit(1);
+        }
     }
 
     FILE *f;
-    if (argc == 3) {
-        if ((f = fopen(argv[2], "r")) == NULL) {
+    if (argc == optind) {
+        f = stdin;
+    } else {
+        if ((f = fopen(argv[optind], "r")) == NULL) {
             perror("fopen");
             exit(1);
         }
-    } else {
-        f = stdin;
     }
 
-    int nlines = atoi(argv[1]);
     int c;
     while ((c = getc(f)) != EOF) {
         putc(c, stdout);
