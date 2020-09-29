cc13ee84432fa945dce8402e42cb544b0f405eb9
yutarofire <yutaro.ono.418@gmail.com>
2020-09-30 04:42:46 +0900

    Implement mkdir command

---
+++ mkdir/mkdir.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "expected a argument\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (mkdir(argv[i], 0777) == -1) {
+            perror(argv[i]);
+            exit(1);
+        }
+    }
+
+    exit(0);
+}
